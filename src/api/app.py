8dfba9621358a1281133a093ce2016b688df124c
Paris-1305 <boyomnzussouodaringsteve@gmail.com>
2025-11-22 19:16:23 +0100

    install cors

--- src/api/app.py
+++ src/api/app.py
@@ -12,7 +12,7 @@ def create_app():
     # Enable CORS for all routes
     #CORS(app)
   # Enable CORS for all routes FROM your frontend
-    CORS(app, resources={r"/api/*": {"origins": "https://learningfy.netlify.app"}})
+    CORS(app, resources={r"/api/*": {"origins": "https://learningfy.netlify.app/"}})
     # Serve OpenAPI spec
     @app.route('/openapi.json')
     def serve_openapi():
