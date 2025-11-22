46165dd8833036a5e14229b3727b55e4f10a12b2
Paris-1305 <boyomnzussouodaringsteve@gmail.com>
2025-11-22 09:37:41 +0100

    fixing env

--- src/config/config.py
+++ src/config/config.py
@@ -1,8 +1,9 @@
-# src/config.py
+from dotenv import load_dotenv
 import os
+
+# Load .env file
+load_dotenv()
 
 class Config:
     DATABASE_PATH = "backend.db"
     VALID_API_KEYS = {os.getenv("API_KEY")}
-
-
