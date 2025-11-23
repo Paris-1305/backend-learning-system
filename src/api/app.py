9b2d1a022f7c211d05f93b465705dec1e0182132
Paris-1305 <boyomnzussouodaringsteve@gmail.com>
2025-11-23 09:53:53 +0100

    fixing he cors issue5

--- src/api/app.py
+++ src/api/app.py
@@ -115,22 +115,16 @@ def create_app():
     # -------------------------------------------------
     # Force-CORS on all responses (fix Render bug)
     # -------------------------------------------------
+    
     @app.after_request
     def add_cors_headers(response):
-        origin = request.headers.get("Origin")
+      response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin")
+      response.headers["Vary"] = "Origin"
+      response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, x-api-key"
+      response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+      response.headers["Access-Control-Allow-Credentials"] = "true"
+      return response
 
-        allowed = [
-            
-            "https://learningfy.netlify.app"
-        ]
-
-        if origin in allowed:
-            response.headers["Access-Control-Allow-Origin"] = origin
-
-        response.headers["Access-Control-Allow-Credentials"] = "true"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, x-api-key"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        return response
 
     # -------------------------------------------------
     # Serve OpenAPI Spec
